7b167bf95072835bd2f1dce2f12d459faac7977a
Abhishek <abhishekroxtomar@gmail.com>
2023-05-09 23:45:41 +0530

    bg update

--- src/app/components/background.tsx
+++ src/app/components/background.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
-import bg from '../assets/weathers/patchy_light_rain_thunder_day_386.png'
+import bg from '../assets/weathers/light_freezing_rain_day_311.png'
 
 const Background = () => {
     //import bg 
