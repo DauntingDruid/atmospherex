d8a82d0c94221e33b0d38286a2ef0a7f76510738
Abhishek <abhishekroxtomar@gmail.com>
2023-05-12 18:47:18 +0530

    inf

--- src/app/components/background.tsx
+++ src/app/components/background.tsx
@@ -5,7 +5,7 @@ import bg from '../assets/weathers/light_freezing_rain_day_311.png'
 const Background = () => {
     //import bg 
         //filter bg according to number
-
+ 
     return (
         <div className='absolute z-[0] h-screen w-screen'>
            <Image
